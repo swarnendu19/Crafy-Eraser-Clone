e6faea24848240865364aa2a5e0b6bda8f9c7c06
Swarnendu <swarnendumaity633@gmail.com>
2024-07-07 17:46:00 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Hello
+      </h1>
+      <Button className="bg-green-600">
+        Hello
+      </Button>
+    </div>
+  );
+}
